7b10c279a7f507310839d1a535dd0f95b4f97348
Akpa Wisdom <80590563+Domkcwis@users.noreply.github.com>
2022-10-24 16:58:40 +0100

    TASK 2-puppet_custom_http_response-header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,25 +1,22 @@
-# Installs a Nginx server with custome HTTP header
+#!/usr/bin/env bash
+# Install and configure HAproxy on your lb-01 server.
 
-exec {'update':
-  provider => shell,
-  command  => 'sudo apt-get -y update',
-  before   => Exec['install Nginx'],
-}
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:vbernat/haproxy-1.8
+apt-get update
+apt-get install -y haproxy=1.8.\*
 
-exec {'install Nginx':
-  provider => shell,
-  command  => 'sudo apt-get -y install nginx',
-  before   => Exec['add_header'],
-}
+load_balancer="\nfrontend http\n
+\tbind *:80
+\tmode http
+\tdefault_backend load_b
 
-exec { 'add_header':
-  provider    => shell,
-  environment => ["HOST=${hostname}"],
-  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
-  before      => Exec['restart Nginx'],
-}
+backend load_b\n
+\tbalance roundrobin
+\tserver web-01 35.185.103.0:80 check
+\tserver web-02 35.237.21.105:80 check
+"
+echo -e "ENABLED=1\n" | sudo tee -a /etc/default/haproxy
+echo -e "$load_balancer" | sudo tee -a /etc/haproxy/haproxy.cfg
 
-exec { 'restart Nginx':
-  provider => shell,
-  command  => 'sudo service nginx restart',
-}
+service haproxy restart
